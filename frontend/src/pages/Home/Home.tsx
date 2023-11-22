7f070b1521367d9e9c4b0bcadf05e8ae447ad069
GiSeong Lee <cj8928@gmail.com>
2023-11-22 23:50:11 +0900

    test

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,10 +1,19 @@
+import { BASE_URL } from '../../api/const';
 import { useRouter } from '../../hooks/useRouter';
-import { Button } from '@mui/material';
+import { Button, Typography, useTheme } from '@mui/material';
 const Home = () => {
   const { routeTo } = useRouter();
+  console.log(BASE_URL);
+  const theme = useTheme();
   return (
     <>
       Home
+      <Typography
+        color="secondary"
+        sx={{ backgroundColor: theme.palette.primary.main }}
+      >
+        TEST
+      </Typography>
       <Button
         onClick={() => {
           routeTo('/login');
