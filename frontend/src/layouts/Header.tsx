c7dda997cfed3d6487b47e8e3f3976232767ecfc
GiSeong Lee <cj8928@gmail.com>
2023-11-26 20:08:45 +0900

    헤더 완성

--- frontend/src/layouts/Header.tsx
+++ frontend/src/layouts/Header.tsx
@@ -1,72 +1,113 @@
-import { AppBar, Avatar, Box, Button, Container, IconButton, Menu, MenuItem, Toolbar, Tooltip, Typography, useTheme } from '@mui/material';
+import {
+  AppBar,
+  Avatar,
+  Box,
+  Button,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Tooltip,
+  Typography,
+  useTheme,
+} from '@mui/material';
 import { useState } from 'react';
 import { useRouter } from '../hooks/useRouter';
+import { User } from '../types/user';
 
-const pages = ['홈', '자유게시판', '일정', '테이블']
+const pages = ['홈', '자유게시판', '일정', '테이블'];
 const settings = ['프로필', '계정', '내가 쓴 글', '로그아웃'];
 
-const Header = () => {
+const Header = ({ userProfile }: { userProfile: User | null }) => {
   const theme = useTheme();
   const { routeTo } = useRouter();
   const [openUser, setOpenUser] = useState<null | HTMLElement>(null);
-  const handleOpenUserIcon = (e:React.MouseEvent<HTMLElement>) => {
+
+  const handleOpenUserIcon = (e: React.MouseEvent<HTMLElement>) => {
     setOpenUser(e.currentTarget);
-  }
+  };
 
   const handleCloseUserIcon = () => {
     setOpenUser(null);
-  }
+  };
 
-  const handleNavigateButton = (e:React.MouseEvent<HTMLElement>) => {
-    switch(e.currentTarget.innerText){
-      case 'MyFootBall':{
+  const handleNavigateButton = (e: React.MouseEvent<HTMLElement>) => {
+    switch (e.currentTarget.innerText) {
+      case 'MyFootBall': {
         routeTo('/');
         break;
       }
-      case '홈':{
+      case '홈': {
         routeTo('/');
         break;
       }
-      case '자유게시판':{
+      case '자유게시판': {
         routeTo('/freeboard');
         break;
       }
-      case '일정':{
+      case '일정': {
         routeTo('/schedule');
         break;
       }
-      case '테이블':{
+      case '테이블': {
         routeTo('/table');
         break;
       }
-      default:{
+      default: {
         break;
       }
     }
-  }
+  };
 
   return (
-    <AppBar position="sticky">
-        <Toolbar>
-          <Box onClick={handleNavigateButton} sx={{font: theme.typography.fontFamily, color: theme.palette.secondary.main, cursor: 'pointer'}}>
-            <Typography variant="h4" sx={{ font: theme.typography.fontFamily }}>
-              MyFootBall
-            </Typography>
-          </Box>
+    <AppBar
+      position="sticky"
+      sx={{
+        minHeight: '100px',
+      }}
+    >
+      <Toolbar>
+        <Box
+          onClick={handleNavigateButton}
+          sx={{
+            cursor: 'pointer',
+          }}
+        >
+          <Typography variant="h3" sx={{ font: theme.typography.fontFamily }}>
+            MyFootBall
+          </Typography>
+        </Box>
 
-
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleNavigateButton}
-                sx={{ my: 2, color: 'white', display: 'block' }}
+        <Box
+          sx={{
+            flexGrow: 1,
+            display: { xs: 'none', md: 'flex' },
+            padding: theme.spacing(1),
+          }}
+        >
+          {pages.map((page) => (
+            <Button
+              key={page}
+              onClick={handleNavigateButton}
+              sx={{
+                my: 2,
+                display: 'block',
+                paddingX: theme.spacing(3),
+              }}
+            >
+              <Typography
+                variant="h6"
+                sx={{
+                  font: theme.typography.fontFamily,
+                  color: theme.palette.secondary.main,
+                }}
               >
                 {page}
-              </Button>
-            ))}
-          </Box>
-
+              </Typography>
+            </Button>
+          ))}
+        </Box>
+        {userProfile !== null ? (
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserIcon} sx={{ p: 0 }}>
@@ -90,15 +131,28 @@ const Header = () => {
               onClose={handleCloseUserIcon}
             >
               {settings.map((setting) => (
-                <MenuItem key={setting} onClick={()=>handleCloseUserIcon}>
+                <MenuItem key={setting} onClick={() => handleCloseUserIcon}>
                   <Typography textAlign="center">{setting}</Typography>
                 </MenuItem>
               ))}
             </Menu>
           </Box>
-        </Toolbar>
-
-
+        ) : (
+          <Box>
+            <Button
+              onClick={() => {
+                routeTo('/login');
+              }}
+              sx={{
+                font: theme.typography.fontFamily,
+                color: theme.palette.secondary.main,
+              }}
+            >
+              로그인
+            </Button>
+          </Box>
+        )}
+      </Toolbar>
     </AppBar>
   );
 };
--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -1,13 +1,26 @@
-import { ReactNode } from 'react';
+import { ReactNode, useEffect, useCallback, useState } from 'react';
 import Footer from './Footer';
 import Header from './Header';
+import { getCurrentUserInfo } from '../api/login';
+import { User } from '../types/user';
 
 const Layout = ({ children }: { children: ReactNode }) => {
+  const [userProfile, setUserProfile] = useState<User | null>(null);
+  const fetchUserProfile = useCallback(async () => {
+    const userProfileResponse = await getCurrentUserInfo();
+    setUserProfile(userProfileResponse);
+  }, []);
+
+  useEffect(() => {
+    console.log('page changed');
+    fetchUserProfile();
+  }, [children]);
+
   return (
     <div
       style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}
     >
-      <Header />
+      <Header userProfile={userProfile} />
       <main style={{ flex: 1 }}>{children}</main>
       <Footer />
     </div>
