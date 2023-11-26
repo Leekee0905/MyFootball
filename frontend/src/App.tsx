64d209fc21986dd357a6bd76e7b857f6da74a0ea
GiSeong Lee <cj8928@gmail.com>
2023-11-26 20:07:06 +0900

    queryprovider추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,16 @@ import { RouterProvider } from 'react-router-dom';
 import { routers } from './router';
 import { ThemeProvider } from '@mui/material';
 import theme from './theme';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <ThemeProvider theme={theme}>
-      <RouterProvider router={routers} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <RouterProvider router={routers} />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
