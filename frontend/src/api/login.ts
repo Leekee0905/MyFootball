9303490f1b99109cef7c0f80a2b3f0329357cb14
GiSeong Lee <cj8928@gmail.com>
2023-11-26 20:08:07 +0900

    로그인테스트

---
+++ frontend/src/api/login.ts
@@ -0,0 +1,22 @@
+//import { User } from '../types/user';
+//import apiInstance from './apiInstance';
+
+/*export const getCurrentUserInfo = async (): Promise<User | null> => {
+  try {
+    const userInfoRes = await apiInstance.get('/profile');
+    console.log(userInfoRes.status);
+    return userInfoRes.status ? userInfoRes.data() : null;
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+};*/
+export const getCurrentUserInfo = async () => {
+  /*return {
+    username: 'string',
+    userInfo: {
+      name: 'hi',
+    },
+  };*/
+  return null;
+};
