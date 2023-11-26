160d6062a610400e5e9d038f1672e83aa73f3b11
GiSeong Lee <cj8928@gmail.com>
2023-11-26 20:08:28 +0900

    푸터 컨테이너 추가

--- frontend/src/layouts/Footer.tsx
+++ frontend/src/layouts/Footer.tsx
@@ -1,4 +1,11 @@
-import { Grid, Typography, Box, Link, useTheme } from '@mui/material';
+import {
+  Grid,
+  Typography,
+  Box,
+  Link,
+  useTheme,
+  Container,
+} from '@mui/material';
 
 const Footer = () => {
   const theme = useTheme();
@@ -11,36 +18,38 @@ const Footer = () => {
         padding: '20px 0',
       }}
     >
-      <Grid container spacing={2}>
-        <Grid item xs={12} md={4}>
-          <Typography variant="h6">About Us</Typography>
-          <Typography variant="body2">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          </Typography>
+      <Container>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={4}>
+            <Typography variant="h6">About Us</Typography>
+            <Typography variant="body2">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Typography variant="h6">Follow Us</Typography>
+            <div>
+              <Link href="#" color="inherit" sx={{ marginRight: 1 }}>
+                f
+              </Link>
+              <Link href="#" color="inherit" sx={{ marginRight: 1 }}>
+                t
+              </Link>
+              <Link href="#" color="inherit" sx={{ marginRight: 1 }}>
+                i
+              </Link>
+              <Link href="#" color="inherit">
+                y
+              </Link>
+            </div>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Typography variant="h6">Contact Us</Typography>
+            <Typography variant="body2">Email: contact@example.com</Typography>
+            <Typography variant="body2">Phone: +1 234 567 890</Typography>
+          </Grid>
         </Grid>
-        <Grid item xs={12} md={4}>
-          <Typography variant="h6">Follow Us</Typography>
-          <div>
-            <Link href="#" color="inherit" sx={{ marginRight: 1 }}>
-              f
-            </Link>
-            <Link href="#" color="inherit" sx={{ marginRight: 1 }}>
-              t
-            </Link>
-            <Link href="#" color="inherit" sx={{ marginRight: 1 }}>
-              i
-            </Link>
-            <Link href="#" color="inherit">
-              y
-            </Link>
-          </div>
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <Typography variant="h6">Contact Us</Typography>
-          <Typography variant="body2">Email: contact@example.com</Typography>
-          <Typography variant="body2">Phone: +1 234 567 890</Typography>
-        </Grid>
-      </Grid>
+      </Container>
     </Box>
   );
 };
