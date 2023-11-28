6e621b9d8e5b864d1b26345da4c064a7bff65d92
GiSeong Lee <cj8928@gmail.com>
2023-11-28 17:00:25 +0900

    홈, 테이블 페이지 구분위한 props추가 및 테이블 구성

--- frontend/src/components/FootballTable.tsx
+++ frontend/src/components/FootballTable.tsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from 'react';
 import apiInstance from '../api/apiInstance';
 import { useQuery } from '@tanstack/react-query';
-import { SeasonTable, TableType } from '../types/table';
+import { SeasonTable } from '../types/table';
 import {
   Box,
+  Button,
   Container,
+  Divider,
   Paper,
   Table,
   TableBody,
@@ -13,7 +15,11 @@ import {
   TableHead,
   TableRow,
   Typography,
+  useMediaQuery,
+  useTheme,
 } from '@mui/material';
+import { useRouter } from '../hooks/useRouter';
+import LoadingCircular from './LoadingCircular';
 
 interface StandingDataType {
   crest: string;
@@ -32,16 +38,6 @@ interface StandingDataType {
   tla: string;
   won: number;
 }
-
-const initialTableState: TableType = {
-  currentMatchDay: 0,
-  emblem: '',
-  leagueid: 0,
-  leagueName: '',
-  season: '',
-  standings: [],
-};
-
 const initialStandingTableData: StandingDataType[] = [
   {
     crest: '',
@@ -62,17 +58,32 @@ const initialStandingTableData: StandingDataType[] = [
   },
 ];
 
-const tableHeader = ['순위', '', '클럽', '경기수', '승', '무', '패', '승점'];
+const league: { [league: string]: string } = {
+  프리미어리그: 'PL',
+  분데스리가: 'BL1',
+  라리가: 'PD',
+  세리에: 'SA',
+  리그앙: 'FL1',
+};
 
-const HomeTable = () => {
-  const [responseData, setResponseData] =
-    useState<TableType>(initialTableState);
+interface TableProps {
+  tableHeader: Array<string>;
+  isHome: boolean;
+}
+
+const FootballTable = ({ tableHeader, isHome }: TableProps) => {
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('md'));
+  const { routeTo } = useRouter();
   const [standingsData, setStandingsData] = useState<StandingDataType[]>(
     initialStandingTableData,
   );
+  const [currentTableLeagueName, setCurrentTableLeaugueName] =
+    useState<string>('PL');
   const { data, isLoading, isSuccess } = useQuery({
-    queryKey: ['table'],
-    queryFn: () => apiInstance.get('/football/epl-standings'),
+    queryKey: ['table', currentTableLeagueName],
+    queryFn: () =>
+      apiInstance.get(`/football/${currentTableLeagueName}/standings`),
     staleTime: Infinity,
     enabled: true,
   });
@@ -86,58 +97,187 @@ const HomeTable = () => {
     return teamData;
   };
 
+  const handleTableHeaderLeaugeClick = (e: React.MouseEvent<HTMLElement>) => {
+    const currentLeague = league[e.currentTarget.innerText];
+    console.log(league[e.currentTarget.innerText]);
+    setCurrentTableLeaugueName(currentLeague);
+  };
+
   useEffect(() => {
     if (isSuccess) {
-      setResponseData(data.data);
       extractingTeamNames(data.data.standings);
-      setStandingsData(extractingTeamNames(data.data.standings));
+
+      isHome
+        ? setStandingsData(
+            extractingTeamNames(data.data.standings.slice(0, 10)),
+          )
+        : setStandingsData(extractingTeamNames(data.data.standings));
     }
   }, [data]);
 
-  return !isLoading ? (
-    <Container>
-      <Paper>
-        <Typography>프리미어리그</Typography>
-        <TableContainer sx={{ maxHeight: '100%' }}>
+  const renderLeagueButtons = () => {
+    return Object.keys(league).map((leagueName, idx) => {
+      const isLeagueActive = league[leagueName] === currentTableLeagueName;
+
+      return (
+        <Button
+          key={idx}
+          onClick={handleTableHeaderLeaugeClick}
+          sx={{
+            backgroundColor: isLeagueActive
+              ? theme.palette.primary.main
+              : 'inherit',
+          }}
+        >
+          <Typography
+            variant="body2"
+            sx={{
+              color: isLeagueActive ? theme.palette.secondary.main : 'inherit',
+            }}
+          >
+            {leagueName}
+          </Typography>
+        </Button>
+      );
+    });
+  };
+
+  return (
+    <Container
+      sx={{
+        marginY: '32px',
+      }}
+    >
+      <Paper
+        sx={{
+          border: isHome ? `1px solid ${theme.palette.primary.main}` : '0',
+          boxShadow: 0,
+        }}
+      >
+        <Box
+          className="table_header"
+          sx={{
+            minHeight: '64px',
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            padding: isSmallScreen ? '16px' : '24px',
+          }}
+        >
+          <Typography variant="h5" sx={{ paddingLeft: '12px' }}>
+            해외축구 순위표
+          </Typography>
+          {isHome ? (
+            <Button onClick={() => routeTo('/table')}>더보기 {'>'}</Button>
+          ) : null}
+        </Box>
+
+        <Divider />
+        <Box
+          className="table_header_league"
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            paddingX: isHome ? '32px' : '128px',
+          }}
+        >
+          {isSmallScreen
+            ? renderLeagueButtons().slice(0, 2)
+            : renderLeagueButtons()}
+        </Box>
+
+        <Divider />
+        <TableContainer className="home_table_container">
           <Table aria-label="home-table" size="small">
             <TableHead>
               <TableRow>
                 {tableHeader.map((e, idx) => {
-                  return <TableCell key={idx}>{e}</TableCell>;
+                  return (
+                    <TableCell sx={{ textAlign: 'center' }} key={idx}>
+                      {e}
+                    </TableCell>
+                  );
                 })}
               </TableRow>
             </TableHead>
             <TableBody>
-              {standingsData.map((e) => {
-                return (
-                  <TableRow
-                    key={e.position}
-                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              {!isLoading ? (
+                standingsData.map((e) => {
+                  return (
+                    <TableRow
+                      key={e.position}
+                      sx={{
+                        '&:last-child td, &:last-child th': {
+                          border: 0,
+                        },
+                      }}
+                    >
+                      <TableCell sx={{ textAlign: 'center' }}>
+                        {e.position}
+                      </TableCell>
+                      <TableCell>
+                        <Box>
+                          <img
+                            src={e.crest}
+                            style={{ maxWidth: '32px', maxHeight: '100%' }}
+                            width="32px"
+                            height="32px"
+                            alt={e.shortName}
+                            decoding="async"
+                            loading="lazy"
+                          ></img>
+                        </Box>
+                      </TableCell>
+                      <TableCell sx={{ textAlign: 'center' }}>
+                        {e.shortName}
+                      </TableCell>
+                      <TableCell sx={{ textAlign: 'center' }}>
+                        {e.playedGames}
+                      </TableCell>
+                      <TableCell sx={{ textAlign: 'center' }}>
+                        {e.won}
+                      </TableCell>
+                      <TableCell sx={{ textAlign: 'center' }}>
+                        {e.draw}
+                      </TableCell>
+                      <TableCell sx={{ textAlign: 'center' }}>
+                        {e.lost}
+                      </TableCell>
+                      {!isHome ? (
+                        <>
+                          <TableCell sx={{ textAlign: 'center' }}>
+                            {e.goalsFor}
+                          </TableCell>
+                          <TableCell sx={{ textAlign: 'center' }}>
+                            {e.goalsAgainst}
+                          </TableCell>
+                          <TableCell sx={{ textAlign: 'center' }}>
+                            {e.goalDifference}
+                          </TableCell>
+                        </>
+                      ) : null}
+                      <TableCell sx={{ textAlign: 'center' }}>
+                        {e.points}
+                      </TableCell>
+                    </TableRow>
+                  );
+                })
+              ) : (
+                <TableRow>
+                  <TableCell
+                    colSpan={tableHeader.length}
+                    sx={{ textAlign: 'center', height: '480px' }}
                   >
-                    <TableCell>{e.position}</TableCell>
-                    <TableCell>
-                      <Box>
-                        <img
-                          src={e.crest}
-                          style={{ maxWidth: '32px', maxHeight: '32px' }}
-                        ></img>
-                      </Box>
-                    </TableCell>
-                    <TableCell>{e.shortName}</TableCell>
-                    <TableCell>{e.playedGames}</TableCell>
-                    <TableCell>{e.won}</TableCell>
-                    <TableCell>{e.draw}</TableCell>
-                    <TableCell>{e.lost}</TableCell>
-                    <TableCell>{e.points}</TableCell>
-                  </TableRow>
-                );
-              })}
+                    <LoadingCircular />
+                  </TableCell>
+                </TableRow>
+              )}
             </TableBody>
           </Table>
         </TableContainer>
       </Paper>
     </Container>
-  ) : null;
+  );
 };
 
-export default HomeTable;
+export default FootballTable;
