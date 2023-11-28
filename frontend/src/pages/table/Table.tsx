fdd2876608f037bc89dec6b509d2ae1b915a076a
GiSeong Lee <cj8928@gmail.com>
2023-11-28 16:59:17 +0900

    테이블 페이지 생성

--- frontend/src/pages/table/Table.tsx
+++ frontend/src/pages/table/Table.tsx
@@ -1,5 +1,19 @@
+import FootballTable from '../../components/FootballTable';
+const tableHeader: Array<string> = [
+  '순위',
+  '',
+  '클럽',
+  '경기수',
+  '승',
+  '무',
+  '패',
+  '득점',
+  '실점',
+  '득실차',
+  '승점',
+];
 const Table = () => {
-  return(<>table</>);
-}
+  return <FootballTable isHome={false} tableHeader={tableHeader} />;
+};
 
 export default Table;
