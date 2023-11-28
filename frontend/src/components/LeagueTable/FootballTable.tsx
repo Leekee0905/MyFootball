c190d17772371e16a98570803b846d02269a9e36
GiSeong Lee <cj8928@gmail.com>
2023-11-28 23:55:15 +0900

    테이블 폴더 생성 및 컴포넌트 분해

--- frontend/src/components/LeagueTable/FootballTable.tsx
+++ frontend/src/components/LeagueTable/FootballTable.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
-import apiInstance from '../api/apiInstance';
+import apiInstance from '../../api/apiInstance';
 import { useQuery } from '@tanstack/react-query';
-import { SeasonTable } from '../types/table';
+import { SeasonTable } from '../../types/table';
 import {
   Box,
   Button,
@@ -18,13 +18,14 @@ import {
   useMediaQuery,
   useTheme,
 } from '@mui/material';
-import { useRouter } from '../hooks/useRouter';
-import LoadingCircular from './LoadingCircular';
+import { useRouter } from '../../hooks/useRouter';
+import LoadingCircular from '../LoadingCircular';
+import LeagueButton from './LeagueButton';
 
 interface StandingDataType {
   crest: string;
   draw: number;
-  form: string | null;
+  form: string;
   goalDifference: number;
   goalsAgainst: number;
   goalsFor: number;
@@ -38,11 +39,17 @@ interface StandingDataType {
   tla: string;
   won: number;
 }
+
+interface TableProps {
+  tableHeader: Array<string>;
+  isHome: boolean;
+}
+
 const initialStandingTableData: StandingDataType[] = [
   {
     crest: '',
     draw: 0,
-    form: null,
+    form: '',
     goalDifference: 0,
     goalsAgainst: 0,
     goalsFor: 0,
@@ -66,47 +73,77 @@ const league: { [league: string]: string } = {
   리그앙: 'FL1',
 };
 
-interface TableProps {
-  tableHeader: Array<string>;
-  isHome: boolean;
-}
+const resultColor: { [result: string]: string } = {
+  W: '#00db74',
+  L: '#e0005e',
+  D: '#c3b3c5',
+};
 
 const FootballTable = ({ tableHeader, isHome }: TableProps) => {
   const theme = useTheme();
   const isSmallScreen = useMediaQuery(theme.breakpoints.down('md'));
+  const isXlScreen = useMediaQuery(theme.breakpoints.up('xl'));
   const { routeTo } = useRouter();
   const [standingsData, setStandingsData] = useState<StandingDataType[]>(
     initialStandingTableData,
   );
-  const [currentTableLeagueName, setCurrentTableLeaugueName] =
+  const [emblem, setEmblem] = useState<string>('');
+  const [season, setSeason] = useState<string>('2023');
+  const [currentTableLeagueName, setCurrentTableLeagueName] =
     useState<string>('PL');
   const { data, isLoading, isSuccess } = useQuery({
     queryKey: ['table', currentTableLeagueName],
     queryFn: () =>
-      apiInstance.get(`/football/${currentTableLeagueName}/standings`),
+      apiInstance.get(`/football/${currentTableLeagueName}/standings`, {
+        params: {
+          season: season,
+        },
+      }),
     staleTime: Infinity,
     enabled: true,
   });
+
+  const preLoadImage = (url: string) => {
+    const img = new Image();
+    img.src = url;
+  };
+
+  const winDrawLoseColorConverter = (result: string, key: number) => {
+    return (
+      <Box
+        key={key}
+        sx={{
+          borderRadius: '100%',
+          backgroundColor: resultColor[result],
+        }}
+      >
+        <Typography sx={{ color: theme.palette.secondary.main }}>
+          {result}
+        </Typography>
+      </Box>
+    );
+  };
 
   const extractingTeamNames = (data: SeasonTable[]): StandingDataType[] => {
     const teamData = data.map((e: SeasonTable): StandingDataType => {
       const { team, ...other } = e;
       const { crest, id, name, shortName, tla } = team;
+      preLoadImage(crest);
       return { ...other, crest, id, name, shortName, tla };
     });
     return teamData;
   };
 
-  const handleTableHeaderLeaugeClick = (e: React.MouseEvent<HTMLElement>) => {
-    const currentLeague = league[e.currentTarget.innerText];
-    console.log(league[e.currentTarget.innerText]);
-    setCurrentTableLeaugueName(currentLeague);
+  const handleTableHeaderLeaugeClick = (leagueName: string) => {
+    const currentLeague = league[leagueName];
+    setCurrentTableLeagueName(currentLeague);
   };
 
   useEffect(() => {
     if (isSuccess) {
-      extractingTeamNames(data.data.standings);
-
+      console.log(data.data);
+      preLoadImage(data.data.emblem);
+      setEmblem(data.data.emblem);
       isHome
         ? setStandingsData(
             extractingTeamNames(data.data.standings.slice(0, 10)),
@@ -120,24 +157,13 @@ const FootballTable = ({ tableHeader, isHome }: TableProps) => {
       const isLeagueActive = league[leagueName] === currentTableLeagueName;
 
       return (
-        <Button
+        <LeagueButton
           key={idx}
           onClick={handleTableHeaderLeaugeClick}
-          sx={{
-            backgroundColor: isLeagueActive
-              ? theme.palette.primary.main
-              : 'inherit',
-          }}
-        >
-          <Typography
-            variant="body2"
-            sx={{
-              color: isLeagueActive ? theme.palette.secondary.main : 'inherit',
-            }}
-          >
-            {leagueName}
-          </Typography>
-        </Button>
+          isActive={isLeagueActive}
+          label={leagueName}
+          isHome={isHome}
+        />
       );
     });
   };
@@ -146,6 +172,9 @@ const FootballTable = ({ tableHeader, isHome }: TableProps) => {
     <Container
       sx={{
         marginY: '32px',
+        maxWidth: '2560px',
+        width: isXlScreen && !isHome ? '1440px' : null,
+        minWidth: isXlScreen && !isHome ? '1440px' : null,
       }}
     >
       <Paper
@@ -164,11 +193,20 @@ const FootballTable = ({ tableHeader, isHome }: TableProps) => {
             padding: isSmallScreen ? '16px' : '24px',
           }}
         >
-          <Typography variant="h5" sx={{ paddingLeft: '12px' }}>
+          <Typography
+            variant="h5"
+            sx={{
+              paddingLeft: '12px',
+              fontWeight: 'bold',
+              color: theme.palette.primary.main,
+            }}
+          >
             해외축구 순위표
           </Typography>
           {isHome ? (
-            <Button onClick={() => routeTo('/table')}>더보기 {'>'}</Button>
+            <Button onClick={() => routeTo('/table')}>
+              <Typography sx={{ fontWeight: 'bold' }}>더보기 {'>'}</Typography>
+            </Button>
           ) : null}
         </Box>
 
@@ -185,10 +223,21 @@ const FootballTable = ({ tableHeader, isHome }: TableProps) => {
             ? renderLeagueButtons().slice(0, 2)
             : renderLeagueButtons()}
         </Box>
-
+        {isHome ? null : (
+          <>
+            <Divider />
+            <Box sx={{ textAlign: 'center' }}>
+              <img src={emblem} />
+            </Box>
+          </>
+        )}
         <Divider />
         <TableContainer className="home_table_container">
-          <Table aria-label="home-table" size="small">
+          <Table
+            aria-label="home-table"
+            size={isHome ? 'small' : 'medium'}
+            style={{ tableLayout: 'fixed' }}
+          >
             <TableHead>
               <TableRow>
                 {tableHeader.map((e, idx) => {
@@ -205,31 +254,46 @@ const FootballTable = ({ tableHeader, isHome }: TableProps) => {
                 standingsData.map((e) => {
                   return (
                     <TableRow
-                      key={e.position}
+                      key={e.shortName}
                       sx={{
                         '&:last-child td, &:last-child th': {
                           border: 0,
                         },
                       }}
                     >
-                      <TableCell sx={{ textAlign: 'center' }}>
+                      <TableCell
+                        sx={{
+                          textAlign: 'center',
+                        }}
+                      >
                         {e.position}
                       </TableCell>
                       <TableCell>
-                        <Box>
+                        <Box sx={{ textAlign: 'center' }}>
                           <img
                             src={e.crest}
-                            style={{ maxWidth: '32px', maxHeight: '100%' }}
-                            width="32px"
-                            height="32px"
+                            style={{ maxWidth: '64px', maxHeight: '100%' }}
+                            width={isHome ? '32px' : '64px'}
+                            height={isHome ? '32px' : '64px'}
                             alt={e.shortName}
                             decoding="async"
                             loading="lazy"
                           ></img>
                         </Box>
                       </TableCell>
-                      <TableCell sx={{ textAlign: 'center' }}>
-                        {e.shortName}
+                      <TableCell
+                        sx={{
+                          textAlign: 'center',
+                        }}
+                      >
+                        <Typography
+                          sx={{
+                            color: theme.palette.primary.main,
+                            fontWeight: 'bold',
+                          }}
+                        >
+                          {isHome ? e.tla : e.shortName}
+                        </Typography>
                       </TableCell>
                       <TableCell sx={{ textAlign: 'center' }}>
                         {e.playedGames}
@@ -257,8 +321,26 @@ const FootballTable = ({ tableHeader, isHome }: TableProps) => {
                         </>
                       ) : null}
                       <TableCell sx={{ textAlign: 'center' }}>
-                        {e.points}
+                        <Typography
+                          sx={{
+                            color: theme.palette.primary.main,
+                            fontWeight: 'bold',
+                          }}
+                        >
+                          {e.points}
+                        </Typography>
                       </TableCell>
+                      {!isHome ? (
+                        <TableCell sx={{ textAlign: 'center' }}>
+                          <Box sx={{ display: 'flex', textAlign: 'center' }}>
+                            {e.form
+                              .split(',')
+                              .map((e, idx) =>
+                                winDrawLoseColorConverter(e, idx),
+                              )}
+                          </Box>
+                        </TableCell>
+                      ) : null}
                     </TableRow>
                   );
                 })
---
+++ frontend/src/components/LeagueTable/LeagueButton.tsx
@@ -0,0 +1,43 @@
+// LeagueButton.tsx
+import React from 'react';
+import { Button, Typography, useTheme } from '@mui/material';
+
+interface LeagueButtonProps {
+  isActive: boolean;
+  onClick: (label: string) => void;
+  label: string;
+  isHome: boolean;
+}
+
+const LeagueButton = ({
+  isActive,
+  onClick,
+  label,
+  isHome,
+}: LeagueButtonProps) => {
+  const theme = useTheme();
+  return (
+    <Button
+      onClick={() => onClick(label)}
+      sx={{
+        backgroundColor: isActive ? theme.palette.primary.main : 'inherit',
+        ':hover': {
+          backgroundColor: theme.palette.primary.main,
+          color: theme.palette.secondary.main,
+        },
+      }}
+    >
+      <Typography
+        variant={isHome ? 'body2' : 'h6'}
+        sx={{
+          color: isActive ? theme.palette.secondary.main : 'inherit',
+          fontWeight: isHome ? null : 'bold',
+        }}
+      >
+        {label}
+      </Typography>
+    </Button>
+  );
+};
+
+export default LeagueButton;
