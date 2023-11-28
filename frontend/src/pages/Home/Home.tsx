799d31a689492bb5a33c0cdd3cc1466fafdd7e71
GiSeong Lee <cj8928@gmail.com>
2023-11-28 16:59:09 +0900

    홈 그리드 비율 조정

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,12 +1,28 @@
 import { Grid } from '@mui/material';
 import HomeTable from '../../components/FootballTable';
-
+const tableHeader: Array<string> = [
+  '순위',
+  '',
+  '클럽',
+  '경기수',
+  '승',
+  '무',
+  '패',
+  '승점',
+];
 const Home = () => {
   return (
     <>
-      <Grid container spacing={2}>
-        <Grid item xs={6} md={4}>
-          <HomeTable />
+      <Grid
+        container
+        spacing={2}
+        sx={{ height: '100%', marginTop: 0, marginLeft: 0 }}
+      >
+        <Grid item xs={6} sm={6} md={6} lg={4} className="home_table_grid">
+          <HomeTable tableHeader={tableHeader} isHome={true} />
+        </Grid>
+        <Grid item xs={6} sm={6} md={6} lg={8}>
+          test
         </Grid>
       </Grid>
     </>
