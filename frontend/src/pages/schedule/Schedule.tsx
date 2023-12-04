9e0be968943d6fecc94f577aa29893a68f57a968
GiSeong Lee <cj8928@gmail.com>
2023-12-05 00:26:55 +0900

    일정 페이지 시작

--- frontend/src/pages/schedule/Schedule.tsx
+++ frontend/src/pages/schedule/Schedule.tsx
@@ -1,5 +1,39 @@
+import { Box, Container } from '@mui/material';
+import { useState, useEffect } from 'react';
+import {
+  league,
+  renderLeagueButtons,
+} from '../../components/RenderLeagueButton';
 const Schedule = () => {
-  return(<>schedule</>);
-}
+  const [currentTableLeagueName, setCurrentTableLeagueName] =
+    useState<string>('PL');
+
+  const handleTableHeaderLeaugeClick = (leagueName: string) => {
+    const currentLeague = league[leagueName];
+    setCurrentTableLeagueName(currentLeague);
+  };
+  useEffect(() => {
+    console.log(currentTableLeagueName);
+  });
+  const utcDateStr = '2023-12-02T15:00:00Z';
+  const utcDate = new Date(utcDateStr);
+
+  // 로컬 시간으로 변환
+  const localDateStr = utcDate.toLocaleString();
+
+  console.log(localDateStr);
+
+  return (
+    <Container>
+      <Box sx={{ textAlign: 'center' }}>
+        {renderLeagueButtons({
+          isHome: false,
+          currentTableLeagueName,
+          onClick: handleTableHeaderLeaugeClick,
+        })}
+      </Box>
+    </Container>
+  );
+};
 
 export default Schedule;
