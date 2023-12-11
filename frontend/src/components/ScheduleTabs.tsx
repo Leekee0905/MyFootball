cb7b037d3feffeb5f7b0334a9f17846f2e95b39b
GiSeong Lee <cj8928@gmail.com>
2023-12-11 17:07:27 +0900

    일정 페이지 완료

--- frontend/src/components/ScheduleTabs.tsx
+++ frontend/src/components/ScheduleTabs.tsx
@@ -1,4 +1,5 @@
 import { Tab, Tabs, Box, Typography } from '@mui/material';
+import React from 'react';
 import { useState } from 'react';
 
 interface TeamType {
@@ -8,56 +9,63 @@ interface TeamType {
 }
 
 interface ScheduleTabs {
-  elements: Array<string | TeamType>;
+  elements: Array<TeamType | string>;
   today?: number;
-  setSelectedTeamId?: (id: number) => void;
+  setSelectedId?: (id: number) => void;
 }
 
-const ScheduleTabs = ({ elements, today, setSelectedTeamId }: ScheduleTabs) => {
-  const [value, setValue] = useState<number>(
-    today !== undefined ? (today < 8 ? today + 4 : today - 8) : 0,
-  );
+const ScheduleTabs = React.memo(
+  ({ elements, today, setSelectedId }: ScheduleTabs) => {
+    const [value, setValue] = useState<number>(
+      today !== undefined ? (today < 8 ? today + 4 : today - 8) : 0,
+    );
 
-  const handleChange = (e: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue);
-    if (setSelectedTeamId && typeof setSelectedTeamId === 'function') {
-      setSelectedTeamId(Number(elements[value]));
-    }
-  };
-  return (
-    <Box className="monthtabs" sx={{ maxWidth: '1150px' }}>
-      <Tabs
-        value={value}
-        onChange={handleChange}
-        variant="scrollable"
-        allowScrollButtonsMobile
-        scrollButtons="auto"
-      >
-        {elements.map((e: TeamType | string, index: number) => {
-          return (
-            <Tab
-              label={
-                typeof e === 'string' ? (
-                  e
-                ) : (
-                  <Box>
-                    <img
-                      src={e.crest}
-                      alt={e.team}
-                      width={'40px'}
-                      height={'40px'}
-                    />
-                    <Typography>{e.team}</Typography>
-                  </Box>
-                )
-              }
-              key={index}
-            />
-          );
-        })}
-      </Tabs>
-    </Box>
-  );
-};
+    const handleChange = (e: React.SyntheticEvent, newValue: number) => {
+      setValue(newValue);
+      if (setSelectedId && typeof setSelectedId === 'function') {
+        const selectedElement = elements[newValue];
+        if (typeof selectedElement !== 'string') {
+          setSelectedId(Number(selectedElement.id));
+        } else {
+          setSelectedId(Number(selectedElement.slice(0, -1)));
+        }
+      }
+    };
+    return (
+      <Box className="monthtabs" sx={{ maxWidth: '1150px' }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          variant="scrollable"
+          allowScrollButtonsMobile
+          scrollButtons="auto"
+        >
+          {elements.map((e: TeamType | string, index: number) => {
+            return (
+              <Tab
+                label={
+                  typeof e === 'string' ? (
+                    e
+                  ) : (
+                    <Box>
+                      <img
+                        src={e.crest}
+                        alt={e.team}
+                        width={'40px'}
+                        height={'40px'}
+                      />
+                      <Typography>{e.team}</Typography>
+                    </Box>
+                  )
+                }
+                key={index}
+              />
+            );
+          })}
+        </Tabs>
+      </Box>
+    );
+  },
+);
 
 export default ScheduleTabs;
