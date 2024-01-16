7451b44ff2095e2aaeac7feb45b331d5941d2062
GiSeong Lee <cj8928@gmail.com>
2024-01-16 17:18:23 +0900

    404페이지 추가

---
+++ frontend/src/pages/error/Error404.tsx
@@ -0,0 +1,40 @@
+import { Container, Box, Typography, Button } from '@mui/material';
+import { useRouter } from '../../hooks/useRouter';
+
+const Error404 = () => {
+  const { routeTo } = useRouter();
+
+  return (
+    <Container
+      sx={{
+        width: '100%',
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column', // 컨테이너의 자식 요소들을 세로로 정렬
+      }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          textAlign: 'center',
+          alignItems: 'center',
+          flexDirection: 'row',
+        }}
+      >
+        <Typography variant="h1" fontWeight={'bold'}>
+          404
+        </Typography>
+        <Typography sx={{ marginLeft: '24px' }} variant="h2">
+          Not Found
+        </Typography>
+      </Box>
+      <Button sx={{ border: '1px solid' }} onClick={() => routeTo('/')}>
+        <Typography variant="h5">홈으로</Typography>
+      </Button>
+    </Container>
+  );
+};
+
+export default Error404;
--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -2,19 +2,14 @@ import { createBrowserRouter } from 'react-router-dom';
 import { Router as RemixRouter } from '@remix-run/router/dist/router';
 import Home from './pages/home/Home';
 import Layout from './layouts/Layout';
-import Login from './pages/login/Login';
-import FreeBoard from './pages/freeboard/FreeBoard';
 import Schedule from './pages/schedule/Schedule';
 import Table from './pages/table/Table';
-import Signup from './pages/signup/Signup';
-import Write from './pages/freeboard/Write';
-
+import Error404 from './pages/error/Error404';
 interface RouterElement {
   id: number;
   path: string;
   label: string;
   element: React.ReactNode;
-  withAuth?: boolean;
 }
 
 const routerData: RouterElement[] = [
@@ -23,49 +18,18 @@ const routerData: RouterElement[] = [
     path: '/',
     label: 'Home',
     element: <Home />,
-    withAuth: false,
   },
   {
     id: 1,
-    path: '/login',
-    label: 'Login',
-    element: <Login />,
-    withAuth: false,
-  },
-  {
-    id: 2,
-    path: '/freeBoard',
-    label: 'FreeBoard',
-    element: <FreeBoard />,
-    withAuth: false,
-  },
-  {
-    id: 3,
     path: '/schedule',
     label: 'Schedule',
     element: <Schedule />,
-    withAuth: false,
   },
   {
-    id: 4,
+    id: 2,
     path: '/table',
     label: 'Table',
     element: <Table />,
-    withAuth: false,
-  },
-  {
-    id: 5,
-    path: '/signup',
-    label: 'Signup',
-    element: <Signup />,
-    withAuth: false,
-  },
-  {
-    id: 6,
-    path: '/write',
-    label: 'Write',
-    element: <Write />,
-    withAuth: false,
   },
 ];
 
@@ -74,6 +38,7 @@ export const routers: RemixRouter = createBrowserRouter(
     return {
       path: router.path,
       element: <Layout>{router.element}</Layout>,
+      errorElement: <Error404 />,
     };
   }),
 );
