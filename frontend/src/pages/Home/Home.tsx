6dd2600b22ad02fa2b4d63045d01d724d7756611
GiSeong Lee <cj8928@gmail.com>
2024-01-16 17:18:03 +0900

    홈 크기 재정렬

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,7 +1,6 @@
 import { Box, Grid, useTheme } from '@mui/material';
 import FootballTable from '../../components/LeagueTable/FootballTable';
 import Schedule from '../schedule/Schedule';
-import FreeBoard from '../freeboard/FreeBoard';
 
 const tableHeader: Array<string> = [
   '순위',
@@ -18,35 +17,36 @@ const Home = () => {
   return (
     <>
       <Grid container spacing={1} sx={{ height: '100%', marginY: '32px' }}>
-        <Grid item xs={6} sm={4} md={4} lg={4} className="home_table_grid">
+        <Grid
+          item
+          xs={6}
+          sm={4}
+          md={4}
+          lg={4}
+          className="home_table_grid"
+          sx={{ maxHeight: '830px' }}
+        >
           <Box
             sx={{
               border: `1px solid ${theme.palette.primary.main}`,
-              height: '100%',
+              maxHeight: '100%',
+              minHeight: '100%',
               overflow: 'auto',
             }}
           >
             <FootballTable tableHeader={tableHeader} isHome={true} />
           </Box>
         </Grid>
-        <Grid item xs={6} sm={8} md={8} lg={8} sx={{ overflow: 'auto' }}>
+        <Grid item xs={6} sm={8} md={8} lg={8} sx={{ maxHeight: '830px' }}>
           <Box
             sx={{
               overflow: 'auto',
-              height: '400px',
+              maxHeight: '100%',
+              minHeight: '100%',
               border: `1px solid ${theme.palette.primary.main}`,
             }}
           >
             <Schedule isHome={true} />
-          </Box>
-          <Box
-            sx={{
-              overflow: 'auto',
-              height: '400px',
-              border: `1px solid ${theme.palette.primary.main}`,
-            }}
-          >
-            <FreeBoard isHome={true} />
           </Box>
         </Grid>
       </Grid>
